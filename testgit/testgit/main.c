1283655ae1b3cca50d2a223af5858046d754f484
Anne Line Gjersem <anne_line94@hotmail.com>
2017-03-19 18:20:58 +0100

    added index.html

---
+++ testgit/testgit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  testgit
+//
+//  Created by Anne Line Gjersem on 2017-03-19.
+//  Copyright © 2017 Anne Line Gjersem. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
